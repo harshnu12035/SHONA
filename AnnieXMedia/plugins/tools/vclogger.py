5f3a7a96708990eae032d04c23cf6470c9717454
hbbb02219-hue <hbbb02219@gmail.com>
2025-12-18 13:36:17 +0530

    Update vclogger.py

--- AnnieXMedia/plugins/tools/vclogger.py
+++ AnnieXMedia/plugins/tools/vclogger.py
@@ -7,9 +7,9 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from pyrogram.raw import functions
-from ShrutiMusic import app
-from ShrutiMusic.utils.database import get_assistant
-from ShrutiMusic.core.mongo import mongodb
+from AnnieXmedia import app
+from AnnieXmedia.utils.database import get_assistant
+from AnnieXmedia.core.mongo import mongodb
 
 """
 ░█▀█░█▀▄░█▀█░█▀█░█▀▄░▀█▀░█▀▀░▀█▀░█▀█░█▀▄░█░█░░░█░░░▀█▀░█▀▀░█▀▀░█▀█░█▀▀░█▀▀
